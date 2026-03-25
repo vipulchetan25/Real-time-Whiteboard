873acbae62f3e49d52de90364f2dbf7c48502d61
Vipul Chetan <vipulchetan.m24@iiits.in>
2026-03-26 01:38:43 +0530

    Initial commit - whiteboard project

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+  });
+
+  // 🔥 broadcast full state
+  socket.on("canvas-update", ({ roomId, strokes }) => {
+    socket.to(roomId).emit("canvas-update", { strokes });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
